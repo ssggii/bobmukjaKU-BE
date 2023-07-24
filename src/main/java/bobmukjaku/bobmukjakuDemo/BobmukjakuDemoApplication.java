f723679b758128c772bae63c00aaa00ba376b232
ssggii <haan7525@gmail.com>
2023-07-24 14:19:20 +0900

    프로젝트 생성 및 초기 설정

---
+++ src/main/java/bobmukjaku/bobmukjakuDemo/BobmukjakuDemoApplication.java
@@ -0,0 +1,13 @@
+package bobmukjaku.bobmukjakuDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BobmukjakuDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BobmukjakuDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/bobmukjaku/bobmukjakuDemo/BobmukjakuDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package bobmukjaku.bobmukjakuDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BobmukjakuDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
