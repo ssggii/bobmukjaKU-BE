f1e84d687c9c69a9db8a8d9d8a5f7c26e2c85d2a
ssggii <haan7525@gmail.com>
2023-07-25 21:45:52 +0900

    MemberRepository 수정_이메일로 회원 찾기 및 회원 여부 판단, 재학생 인증 여부 판단

--- src/main/java/bobmukjaku/bobmukjakuDemo/domain/member/repository/MemberRepository.java
+++ src/main/java/bobmukjaku/bobmukjakuDemo/domain/member/repository/MemberRepository.java
@@ -3,12 +3,13 @@ package bobmukjaku.bobmukjakuDemo.domain.member.repository;
 import bobmukjaku.bobmukjakuDemo.domain.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import javax.swing.text.html.Option;
+import java.time.LocalDate;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
-    Optional<Member> findByMemberId(String memberId); // 아이디로 회원 찾기
-    boolean existsByMemberId(String memberId); // 아이디로 회원 여부 판단
+    Optional<Member> findByMemberEmail(String memberEmail); // 이메일로 회원 찾기
+    boolean existsByMemberEmail(String memberEmail); // 이메일로 회원 여부 판단
+    boolean ifCertificated(String certificatedAt); // 재학생 인증 여부 판단
 
 }
