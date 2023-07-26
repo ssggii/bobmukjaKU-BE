9096ec5dc0773c03d123e5e040ad19681cc1a412
ssggii <haan7525@gmail.com>
2023-07-26 16:35:17 +0900

    로그인 커스텀 시작

--- src/main/java/bobmukjaku/bobmukjakuDemo/global/config/SecurityConfig.java
+++ src/main/java/bobmukjaku/bobmukjakuDemo/global/config/SecurityConfig.java
@@ -1,12 +1,15 @@
 package bobmukjaku.bobmukjakuDemo.global.config;
 
+import bobmukjaku.bobmukjakuDemo.global.login.filter.JsonUsernamePasswordAuthenticationFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.authorization.AuthorizationManager;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
-import org.springframework.security.config.annotation.web.configurers.AuthorizeHttpRequestsConfigurer;
 import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
 import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
 import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
@@ -16,16 +19,18 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
 
-    /* 세부적인 보안 기능을 설정
-       - 리소스(URL) 접근 권한 설정
-       - 커스텀 로그인 페이지 지원
-       - 인증 후 성공/실패 핸들링
-       - 사용자 로그아웃
-       - CSRF 공격으로 부터 보호
-    */
+    private final ObjectMapper objectMapper;
+
+    /* 특정 요청들을 무시하고 싶을 때 사용 (보안 필터를 적용할 필요가 없는 리소스 설정) */
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/login", "/signUp", "/"); // 로그인, 회원가입 페이지 인증 없이 접근 가능
+    }
+
+    /* 세부적인 보안 기능을 설정 (authorization, authentication) */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
         http
@@ -36,22 +41,32 @@ public class SecurityConfig {
                         httpSecuritySessionManagementConfigurer
                                 -> httpSecuritySessionManagementConfigurer
                                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
-/*
-                .authorizeHttpRequests(
+/*                .authorizeHttpRequests(
                         AuthorizationManager -> AuthorizationManager
-                                .requestMatchers("/login", "/signUp", "/").permitAll());
-*/
+                                .requestMatchers("/login", "/signUp", "/").permitAll()
+                                .anyRequest().authenticated());*/
 
         return http.build();
     }
     @Bean
-    public PasswordEncoder passwordEncoder(){
+    public PasswordEncoder passwordEncoder(){ // 1. PasswordEncoder 등록
         return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 
-    /* 특정 요청들을 무시하고 싶을 때 사용 (보안 필터를 적용할 필요가 없는 리소스 설정) */
     @Bean
-    public WebSecurityCustomizer webSecurityCustomizer() {
-        return (web) -> web.ignoring().requestMatchers("/"); // 로그인 페이지 인증 없이 접근 가능
+    public AuthenticationManager authenticationManager() {// 2. AuthenticationManager 등록
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();// DaoAuthenticationProvider 사용
+        provider.setPasswordEncoder(passwordEncoder());// PasswordEncoder로는 PasswordEncoderFactories.createDelegatingPasswordEncoder() 사용
+        //provider.setUserDetailsService(loginService);
+        return new ProviderManager(provider);
+    }
+
+    @Bean
+    public JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter(){
+        JsonUsernamePasswordAuthenticationFilter jsonUsernamePasswordLoginFilter = new JsonUsernamePasswordAuthenticationFilter(objectMapper);
+        jsonUsernamePasswordLoginFilter.setAuthenticationManager(authenticationManager());
+        //jsonUsernamePasswordLoginFilter.setAuthenticationSuccessHandler(loginSuccessJWTProvideHandler());
+        //jsonUsernamePasswordLoginFilter.setAuthenticationFailureHandler(loginFailureHandler());//변경
+        return jsonUsernamePasswordLoginFilter;
     }
 }
