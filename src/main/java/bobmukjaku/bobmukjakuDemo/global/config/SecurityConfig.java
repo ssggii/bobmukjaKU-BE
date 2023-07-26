3af5e669987013099fcadd5b50077bbef81142ee
ssggii <haan7525@gmail.com>
2023-07-26 14:09:06 +0900

    SecurityConfig 수정 1차

--- src/main/java/bobmukjaku/bobmukjakuDemo/global/config/SecurityConfig.java
+++ src/main/java/bobmukjaku/bobmukjakuDemo/global/config/SecurityConfig.java
@@ -1,8 +1,36 @@
 package bobmukjaku.bobmukjakuDemo.global.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.CsrfConfigurer;
+import org.springframework.security.config.annotation.web.configurers.FormLoginConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HttpBasicConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http
+                .formLogin(FormLoginConfigurer::disable) // 1. formLogin 비활성화
+                .httpBasic(HttpBasicConfigurer::disable) // 2. httpBasic 인증 비활성화 (특정 리소스에 접근 시 username과 password 물어봄)
+                .csrf(CsrfConfigurer::disable) // 3. csrf 비활성화
+                .sessionManagement( // 4. 세션 Stateless로 유지
+                        httpSecuritySessionManagementConfigurer
+                                -> httpSecuritySessionManagementConfigurer
+                                .sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+        return http.build();
+    }
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
 }
