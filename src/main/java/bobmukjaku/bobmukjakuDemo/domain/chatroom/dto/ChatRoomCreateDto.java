639fb809c50906d85ab2e5c3f8563f5e2e520e21
ssggii <haan7525@gmail.com>
2023-08-07 12:27:25 +0900

    ChatRoomCreateDto 생성

---
+++ src/main/java/bobmukjaku/bobmukjakuDemo/domain/chatroom/dto/ChatRoomCreateDto.java
@@ -0,0 +1,32 @@
+package bobmukjaku.bobmukjakuDemo.domain.chatroom.dto;
+
+import bobmukjaku.bobmukjakuDemo.domain.chatroom.ChatRoom;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+public record ChatRoomCreateDto(String roomName, String date,
+                                String startTime, String endTime, String kindOfFood, Integer total) {
+
+    public ChatRoom toEntity(){
+        ChatRoom chatRoom = ChatRoom.builder().roomName(roomName)
+                .meetingDate(LocalDate.parse(date))
+                .startTime(LocalTime.parse(startTime))
+                .endTime(LocalTime.parse(endTime))
+                .kindOfFood(kindOfFood)
+                .total(total)
+                .build();
+        return chatRoom;
+    }
+}
+
+
+/*
+*
+* public Member toEntity(){
+        return Member.builder()
+                .memberEmail(memberEmail)
+                .memberPassword(password)
+                .memberNickName(nickname)
+                .build();
+    }*/
