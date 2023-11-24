a685a9e9c9bb75b5288f2f8a66bfb90f701e0b3f
ssggii <haan7525@gmail.com>
2023-11-24 15:11:54 +0900

    스크랩 수 같은 경우 placeId 알파벳순 정렬 추가

--- src/main/java/bobmukjaku/bobmukjakuDemo/domain/place/repository/ScrapRepository.java
+++ src/main/java/bobmukjaku/bobmukjakuDemo/domain/place/repository/ScrapRepository.java
@@ -11,9 +11,10 @@ public interface ScrapRepository extends JpaRepository<Scrap, Long> {
 
     List<Scrap> findAllByPlaceId(String placeId); // 음식점 id로 스크랩 조회
     Long countByPlaceId(String placeId); // 특정 음식점의 스크랩 수 조회
+
     @Query("SELECT s.placeId, COUNT(s.placeId) AS scrapCount " +
             "FROM Scrap s " +
             "GROUP BY s.placeId " +
-            "ORDER BY COUNT(s.placeId) DESC")
+            "ORDER BY COUNT(s.placeId) DESC, s.placeId ASC")
     List<Object[]> findTop10PlacesByScrapCount(); // 상위 스크랩 음식점 조회
 }
--- src/test/java/bobmukjaku/bobmukjakuDemo/domain/place/repository/PlaceRepositoryTest.java
+++ src/test/java/bobmukjaku/bobmukjakuDemo/domain/place/repository/PlaceRepositoryTest.java
@@ -84,4 +84,49 @@ public class PlaceRepositoryTest {
         assertThat(placeId4).isEqualTo("place4");
 
     }
+
+    @Test
+    public void 스크랩수_같은경우_placeId알파벳순_상위스크랩_반환_성공() throws Exception{
+        // given
+        Member member = Member.builder().memberEmail("member1@konkuk.ac.kr").memberPassword("member1@").memberNickName("닉네임1").role(Role.USER).build();
+        memberRepository.save(member);
+        Scrap scrap1 = Scrap.builder().placeId("place1").placeName("음식점1").member(member).build();
+        Scrap scrap2 = Scrap.builder().placeId("place1").placeName("음식점1").member(member).build();
+        Scrap scrap3 = Scrap.builder().placeId("place1").placeName("음식점1").member(member).build();
+        Scrap scrap4 = Scrap.builder().placeId("place2").placeName("음식점2").member(member).build();
+        Scrap scrap5 = Scrap.builder().placeId("place2").placeName("음식점2").member(member).build();
+        Scrap scrap6 = Scrap.builder().placeId("place2").placeName("음식점2").member(member).build();
+        Scrap scrap7 = Scrap.builder().placeId("alace3").placeName("음식점3").member(member).build();
+        Scrap scrap8 = Scrap.builder().placeId("alace3").placeName("음식점3").member(member).build();
+        Scrap scrap9 = Scrap.builder().placeId("alace3").placeName("음식점3").member(member).build();
+        Scrap scrap10 = Scrap.builder().placeId("place1").placeName("음식점1").member(member).build();
+        scrapRepository.save(scrap1);
+        scrapRepository.save(scrap2);
+        scrapRepository.save(scrap3);
+        scrapRepository.save(scrap4);
+        scrapRepository.save(scrap5);
+        scrapRepository.save(scrap6);
+        scrapRepository.save(scrap7);
+        scrapRepository.save(scrap8);
+        scrapRepository.save(scrap9);
+        scrapRepository.save(scrap10);
+
+        // when
+        List<Object[]> result = scrapRepository.findTop10PlacesByScrapCount();
+
+        // then
+        for (Object[] row : result) {
+            String placeId = (String) row[0];
+            System.out.println(placeId);
+        }
+
+        String placeId1 = (String) result.get(0)[0];
+        String placeId2 = (String) result.get(1)[0];
+        String placeId3 = (String) result.get(2)[0];
+
+        assertThat(placeId1).isEqualTo("place1");
+        assertThat(placeId2).isEqualTo("alace3");
+        assertThat(placeId3).isEqualTo("place2");
+
+    }
 }
