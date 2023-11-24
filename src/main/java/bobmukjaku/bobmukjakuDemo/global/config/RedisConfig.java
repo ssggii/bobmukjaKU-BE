de97f0a479a7a26f910394de7eb95bb2a09e7c4f
ssggii <haan7525@gmail.com>
2023-11-24 14:56:53 +0900

    로컬 테스트 Redis 설정 수정

--- src/main/java/bobmukjaku/bobmukjakuDemo/global/config/RedisConfig.java
+++ src/main/java/bobmukjaku/bobmukjakuDemo/global/config/RedisConfig.java
@@ -5,8 +5,10 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
@@ -19,13 +21,19 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(host);
+        redisStandaloneConfiguration.setPort(port);
+        LettuceConnectionFactory lettuceConnectionFactory = new LettuceConnectionFactory(redisStandaloneConfiguration);
+        return lettuceConnectionFactory;
     }
 
     @Bean
     public RedisTemplate<?,?> redisTemplate() {
         RedisTemplate<?,?> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory());
+        redisTemplate.setConnectionFactory(redisConnectionFactory());// connection
+        redisTemplate.setKeySerializer(new StringRedisSerializer());// key
+        redisTemplate.setValueSerializer(new StringRedisSerializer());// value
         return redisTemplate;
     }
 }
